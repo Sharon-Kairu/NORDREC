5a5c1a2848359d5e78bac39f32812e76328802c3
Sharon-Kairu <sharonkairu62@gmail.com>
2025-10-06 21:01:07 +0300

    Changes Trial

--- app/components/Mission/Mission.tsx
+++ app/components/Mission/Mission.tsx
@@ -41,14 +41,14 @@ const Mission = () => {
             <div  className='m-8 flex flex-col gap-8 items-center justify-items-center px-10 py-20 mb-16'>
 
                 <div className='bg-brown-200 w-full flex flex-col items-center justify-center p-4'>
-                    <h1 className='text-3xl font-bold leading-tight text-green-700'>
+                    <h1 className='text-xl md:text-3xl font-bold leading-tight text-green-700'>
                         Our Mission
                     </h1>
                     <div className='mt-4 w-30 h-2 rounded-2xl bg-green-700'></div>
                 </div>
 
                 <div className='flex flex-col space-y-4 '>
-                    <p className='text-xl leading-relaxed text-justify'>
+                    <p className='text-md md:text-xl leading-relaxed text-justify'>
                         To empower dryland agro-pastoralists and farmers' cooperatives with knowledge and skills for
                         Sustainable farming and ecosystem regeneration
                     </p>
@@ -91,14 +91,14 @@ const Mission = () => {
             <div id='mission' className='m-8 flex flex-col gap-8 items-center justify-items-center px-10 py-20'>
 
                 <div className='w-full flex flex-col items-center justify-center p-4'>
-                    <h1 className='text-3xl font-bold leading-tight text-green-700'>
+                    <h1 className='text-xl md:text-3xl font-bold leading-tight text-green-700'>
                         Our Vision
                     </h1>
                     <div className='mt-4 w-30 h-2 rounded-2xl bg-green-700'></div>
                 </div>
 
                 <div className='flex flex-col space-y-4 '>
-                    <p className='text-xl leading-relaxed text-justify'>
+                    <p className='text-md md:text-xl leading-relaxed text-justify'>
                         To be a leading resource center for dryland regeneration across Northern Kenya, promoting sustainable
                         and regenerative agriculture through education, research, and partnerships.
 
